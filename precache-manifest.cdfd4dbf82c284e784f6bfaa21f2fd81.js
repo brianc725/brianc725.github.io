9c5cd500f8ff456132f2f47cb26b8d767dd161e0
Brian Chan <brianc725@gmail.com>
2020-01-26 11:25:47 -0800

    Updates

---
+++ precache-manifest.cdfd4dbf82c284e784f6bfaa21f2fd81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "562faf4217a72a040e209716b3cd99e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "303c8a6cb07401c738c6",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "565209fadbb6140a272b",
+    "url": "/static/css/main.aca72fe3.chunk.css"
+  },
+  {
+    "revision": "303c8a6cb07401c738c6",
+    "url": "/static/js/2.503ba7df.chunk.js"
+  },
+  {
+    "revision": "565209fadbb6140a272b",
+    "url": "/static/js/main.7064c5ee.chunk.js"
+  },
+  {
+    "revision": "5d06a2f0096c931eac3b",
+    "url": "/static/js/runtime~main.b52d1889.js"
+  }
+]);
