93edc3dd8c71fe1424a293db36f8a8d64d4a1816
Brian Chan <brianc725@gmail.com>
2020-08-29 13:52:44 -0700

    Update styling of tools on projects page to use pills

--- src/screens/projects.js
+++ src/screens/projects.js
@@ -3,6 +3,7 @@ import {
   ListGroup,
   ListGroupItem,
   Alert,
+  Badge,
   Card,
   CardLink,
   CardFooter,
@@ -11,6 +12,7 @@ import {
   CardSubtitle,
   CardHeader,
 } from 'reactstrap';
+import { stringToArr } from '../scripts/strings';
 
 class Projects extends Component {
   componentDidMount() {
@@ -36,10 +38,23 @@ class Projects extends Component {
       const descriptionArray = item.description && item.description.split(':');
       let descriptionTools = '';
       let descriptionText = item.description || '';
+      let toolsArray = [];
       if (descriptionArray && descriptionArray.length === 2) {
         descriptionTools = descriptionArray[0].trim();
         descriptionText = descriptionArray[1].trim();
+
+        // Remove square brackets from the string list
+        descriptionTools = descriptionTools.substring(1, descriptionTools.length-1);
+        toolsArray = stringToArr(descriptionTools);
       }
+
+    let descriptionBadges = toolsArray && toolsArray.map((name) => {
+      return (
+        <Badge color="primary" key={name} pill className='badge'>
+          {name}
+        </Badge>
+      );
+    });
 
       return (
         <ListGroupItem key={item.id}>
@@ -47,7 +62,7 @@ class Projects extends Component {
             {item.name && <CardHeader tag="h3" className="text-center">{item.name}</CardHeader>}
             <CardBody className="text-center">
               {descriptionText && <CardTitle tag="h5">{descriptionText}</CardTitle>}
-              {descriptionTools && <CardSubtitle>Utilized: {descriptionTools.substring(1,descriptionTools.length-1)}</CardSubtitle>}
+              {descriptionTools && <CardSubtitle>{descriptionBadges}</CardSubtitle>}
             </CardBody>
             <CardFooter className="text-center">
               {item.html_url && <CardLink href={item.html_url} target="_blank" rel="noopener noreferrer">Github Repository</CardLink>}
